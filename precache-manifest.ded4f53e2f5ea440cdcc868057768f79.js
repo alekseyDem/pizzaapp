dfaf2201fe60b5d01c301070df24f3ef74d46514
Aleksei Dementyev <aleksei.dementyev@altruist.com>
2020-07-19 21:11:47 +0300

    Updates

---
+++ precache-manifest.ded4f53e2f5ea440cdcc868057768f79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71be1f441530c003f0b5ff2a2abd23c",
+    "url": "/pizzaapp/index.html"
+  },
+  {
+    "revision": "3f837f8d438e21f566ae",
+    "url": "/pizzaapp/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "528049972b753623a4f7",
+    "url": "/pizzaapp/static/css/main.a8c24d6a.chunk.css"
+  },
+  {
+    "revision": "3f837f8d438e21f566ae",
+    "url": "/pizzaapp/static/js/2.c0c1723b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pizzaapp/static/js/2.c0c1723b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528049972b753623a4f7",
+    "url": "/pizzaapp/static/js/main.4ca1967f.chunk.js"
+  },
+  {
+    "revision": "25a4887ef54069fecb36",
+    "url": "/pizzaapp/static/js/runtime-main.3c3516b2.js"
+  },
+  {
+    "revision": "0707541983e708cd1825521c3f89e92a",
+    "url": "/pizzaapp/static/media/istockphoto-938742222-1024x1024.07075419.jpg"
+  }
+]);
